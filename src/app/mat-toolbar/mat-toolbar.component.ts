abdf0895c61e60415742bd170bfb3ac2a96239ef
USER01\kanno <kanno062316@icloud.com>
2024-06-09 15:34:17 +0900

    ツールバーの作成（マテリアルの試し）

---
+++ src/app/mat-toolbar/mat-toolbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mat-toolbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './mat-toolbar.component.html',
+  styleUrl: './mat-toolbar.component.css'
+})
+export class MatToolbarComponent {
+
+}
